dd6a5fb238c81976cb84c8565a0a1bab3d1c28ae
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-20 09:06:19 -0700

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -37,7 +37,6 @@ async def help_user(bot, update):
         text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
     )
 
 
@@ -51,7 +50,6 @@ async def get_me_info(bot, update):
         text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
     )
 
 
@@ -71,6 +69,5 @@ async def upgrade(bot, update):
         chat_id=update.chat.id,
         text=Translation.UPGRADE_TEXT,
         parse_mode="html",
-        reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
