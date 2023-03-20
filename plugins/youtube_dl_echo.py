04d53bd6daf3200025609a4eda8b2085948ded79
av6387 <98997680+av6387@users.noreply.github.com>
2023-03-20 09:51:55 -0700

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -276,7 +276,7 @@ async def echo(bot, update):
             None,  # bot,
             Translation.DOWNLOAD_START,
             #update.message.message_id,
-            update.chat.id
+           # update.chat.id
         )
         if os.path.exists(thumb_image_path):
             im = Image.open(thumb_image_path).convert("RGB")
